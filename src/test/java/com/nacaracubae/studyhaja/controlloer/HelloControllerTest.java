a14704db015872385ec87be0edee9325a3134721
withsion6928 <withsion6928@gmail.com>
2022-02-12 21:44:09 +0900

    22

--- src/test/java/com/nacaracubae/studyhaja/controlloer/HelloControllerTest.java
+++ src/test/java/com/nacaracubae/studyhaja/controlloer/HelloControllerTest.java
@@ -2,7 +2,7 @@ package com.nacaracubae.studyhaja.controlloer;
 
 
 import com.nacaracubae.studyhaja.controller.HelloController;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,8 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -29,5 +30,17 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect( jsonPath("$.name", is(name)))
+                .andExpect( jsonPath("$.amount", is(amount)));
+    }
 
 }
